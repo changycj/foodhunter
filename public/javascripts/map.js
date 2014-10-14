db09ad0fae01ec1940b8d8be8401b6163ffe159c
Yachi Chang <changycj@mit.edu>
2014-10-14 19:32:22 -0400

    switched to mapbox instead of polymaps

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,18 +1,8 @@
 $(document).ready(function() {
-    
-    var po = org.polymaps;
 
-    var map = po.map().container($("#map")[0]);
+    L.mapbox.accessToken = "pk.eyJ1IjoiZm9vZGh1bnRlcnMiLCJhIjoiR0laWjlaUSJ9.CtACBQ0c6_gH9I25-Jpj-Q";
     
-    map.center({lat: 42.3585, lon: -71.0935})
-        .zoom(15)
-        .zoomRange([15, 18])
-        .extent([map.pointLocation({x: 0, y: 0}), map.pointLocation(map.size())])
-        .add(po.interact());
-    
-    map.add(po.image()
-        .url(po.url("http://api.tiles.mapbox.com/v4/foodhunters.jp343j89/{Z}/{X}/{Y}.png" 
-                    + "?access_token=pk.eyJ1IjoiZm9vZGh1bnRlcnMiLCJhIjoiR0laWjlaUSJ9.CtACBQ0c6_gH9I25-Jpj-Q")
-        .hosts(["a.", "b.", "c.", ""])));
+    var map = L.mapbox.map("map", "foodhunters.jp343j89")
+        .setView([42.3585, -71.0935], 15);
 
 });
