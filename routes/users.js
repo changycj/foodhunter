9feeb2481ba211f970ce5037c1bfa644c3f9a6cd
Rebekah Cha <rcha@mit.edu>
2014-10-18 14:55:27 -0400

    added users in routes

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,20 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
-var location = require("../models/Location");
+// var location = require("../models/Location");
 var request = require("request");
 
 
-var setupRoutes = function() {
+// var setupRoutes = function() {
     
-    router.get("/", function(req, res) {
+//     router.get("/", function(req, res) {
         
-        location.Location.find({}).sort({building: "desc"}).exec(function(err, locs) {
-            res.render("map", {
-                title: "Food Hunter",
-                locs: locs
-            });
-        });
-    });
-}
+//         location.Location.find({}).sort({building: "desc"}).exec(function(err, locs) {
+//             res.render("map", {
+//                 title: "Food Hunter",
+//                 locs: locs
+//             });
+//         });
+//     });
+// }
 module.exports = router;
