e1bfdc93a032fedb8ea1861764a9899d4035fb77
Dana <mukushev@mit.edu>
2014-10-18 16:52:01 -0400

    routing logic/api for events

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 var mongoose = require("mongoose");
 
 var userSchema = mongoose.Schema({
-	events: [{type: mongoose.Schema.Types.ObjectID, ref: 'Event'}],
-	subscriptions: [{type: mongoose.Schema.Types.ObjectID, ref: 'Subscription'}]
+	kerberos: String,
+	events: [{type: mongoose.Schema.Types.ObjectId, ref: 'Event'}],
+	subscriptions: [{type: mongoose.Schema.Types.ObjectId, ref: 'Subscription'}]
 });
 
 var User = mongoose.model("User", userSchema);
--- routes/events.js
+++ routes/events.js
@@ -2,8 +2,9 @@ var express = require('express');
 var mongoose = require("mongoose");
 var router = express.Router();
 var Event = require('../models/Event').Event;
+var User = require("../models/User").User;
 
-/* DISPLAY ALL EVENTS*/
+/*********DISPLAY ALL EVENTS*********/
 router.get('/', function(req, res) {
 	Event.find({},{}, function(err, doc){
 	if (err){
@@ -16,27 +17,32 @@ router.get('/', function(req, res) {
 	});
 
 });
-/*CREATE A NEW EVENT*/
+
+/*********CREATE A NEW EVENT*********/
+
 /*TODO: validate hours, date, status, error handling in general*/
+//TODO: add event to Users.events list - DONE, but need to be tested
 router.post('/', function(req, res) {
 	var status = "Food"; //default status
-    var host = req.body.host;
-    var startTimeHour = parseInt(req.body.startTimeHour);
-    var startTimeMin = parseInt(req.body.startTimeMin);
-    var startTime = startTimeHour+startTimeMin/60;
+    var host = req.body.host; //kerberos, string!!!!
+    var startTimeHour = req.body.startTimeHour; //number
+    var startTimeMin = req.body.startTimeMin;  //number
+    var startTime = startTimeHour+startTimeMin/60; 
     var endTimeHour = startTimeHour+1; //by default the event is 1 hr long
     if (req.body.endTimeHour){
-    	 endTimeHour= parseInt(req.body.endTimeHour);
+    	 endTimeHour= req.body.endTimeHour;//number
     }
-    var endTimeMin = startTimeMin;
+    var endTimeMin = startTimeMin; 
     if (req.body.endTimeMin){
-    	 endTimeMin= parseInt(req.body.endTimeMin);
+    	 endTimeMin= req.body.endTimeMin; //number
     }
     var endTime = endTimeHour+endTimeMin/60;
 
     var date = new Date(req.body.date);
-    var location = req.body.location;
+    var location = req.body.location; //needs to be ObjectID
+
     var description = req.body.description;
+
     if (description){ //description was included
     	var newEventJSON = {"host":host, 
     					"when": 
@@ -54,7 +60,7 @@ router.post('/', function(req, res) {
     							"date":date},
     					"location": location,
     					"status":status
-    					}
+    					};
     	}
     var newEvent = new Event(newEventJSON);
     newEvent.save(function(err){
@@ -62,12 +68,55 @@ router.post('/', function(req, res) {
     		console.log("Error creating a new event instance");
     	}
     	else{
-    		//res.json(newEvent);
-    		//res.json({message:"event created"}); //smth...
-    		res.redirect('/events');
+    		User.findOne({kerberos:host}, function(err, user){
+    			if (err){
+    				console.log("Error adding an event to the User.events");
+    			}
+    			//IF SUCH USER EXISTS
+    			else  if (user){
+    				user.events.push(newEvent._id);
+    				user.save(function(err){
+    					if (err){
+    						console.log("Error adding an event to the User.events 2");
+    					}
+    					else{
+    						//res.json(newEvent);
+    						//res.json({message:"event created"}); //smth...
+    						res.redirect('/events');
+    					}
+    				});
+    			}
+    			//NO SUCH USER IN THE DATABASE YET
+    			else{
+    				var newUser = new User({"kerberos": host, "events": [newEvent._id]});
+    				newUser.save(function(err){
+    					if (err){
+    						console.log("Error creating a new user instance");
+    					}
+    					else{
+    						res.redirect('/events');
+    					}
+    				});
+    			}
+    		});
     	}
     });
 });
+
+/*********GET individual event info*********/
+router.get('/:eventId', function(req,res){
+	var eventId = req.params.eventId;
+	Event.findById(eventId, function(err, doc){
+		if (err){
+			console.log("Error finding the event");
+			return;
+		}
+		res.send(doc); //just send a JSON object
+	});
+});
+
+/*********UPDATE current event*********/
+//TODO: need to be tested
 router.put('/:eventId', function(req,res){
 	var eventId = req.params.eventId;
 	var fieldToChange = req.body.fieldToChange; 
@@ -106,12 +155,16 @@ router.put('/:eventId', function(req,res){
 				console.log("Error saving the updates");
 				return;
 			}
-			res.redirect('/event/'+eventId);
+			res.send(doc);
+			//res.redirect('/event/'+eventId);
 			//res.json({message:"event updated"});
-		})
+		});
 	});
 });
 
+/*********DELETE an event*********/
+//TODO: delete event from Users.events list - DONE, but needs to be tested
+//
 router.delete('/events/:eventId', function(req,res){
 	var eventId = req.params.eventId;
 	Events.remove({_id:eventId}, function(err){
@@ -119,11 +172,19 @@ router.delete('/events/:eventId', function(req,res){
 			console.log("Error deleting event: "+ eventId);
 		}
 		else{
-			//res.json({message:"event deleted"});
-			res.redirect('/events');
+			User.update({kerberos:host}, {$pull:{events:eventId}}, function(err, doc){
+				if (err){
+					console.log("Error while deleting event from usr's list");
+				}
+				else{
+					//res.json({message:"event deleted"});
+					res.redirect('/events');				}
+			});
+			
 		}
 	});
 
 });
+
 
 module.exports = router;
