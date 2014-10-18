38c1054bc2ac97406afeb1d7b9b5aec309ec342d
Rebekah Cha <rcha@mit.edu>
2014-10-18 15:42:42 -0400

    started api for subscriptions

--- models/User.js
+++ models/User.js
@@ -1,13 +1,6 @@
 var mongoose = require("mongoose");
 
 var userSchema = mongoose.Schema({
-//     gps: {
-//         lat: {type: Number, required: true},
-//         lon: {type: Number, required: true} 
-//     },
-//     building: {type: String },
-//     name: {type: String }
-// });
 	events: [{type: mongoose.Schema.Types.ObjectID, ref: 'Event'}],
 	subscriptions: [{type: mongoose.Schema.Types.ObjectID, ref: 'Subscription'}]
 });
--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,32 @@ var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
 // var location = require("../models/Location");
-var request = require("request");
+var user = require("../models/User.js");
+var subscription = require("../models/Subscription.js");
 
 
-// var setupRoutes = function() {
-    
-//     router.get("/", function(req, res) {
-        
-//         location.Location.find({}).sort({building: "desc"}).exec(function(err, locs) {
-//             res.render("map", {
-//                 title: "Food Hunter",
-//                 locs: locs
-//             });
-//         });
-//     });
-// }
+/*GET subscriptions for the user */
+router.get("/:userID/subscriptions", function(req, res){
+	//find users by :userID
+	var userID = req.params.userID;
+	user.User.findOne({'_id': userID}, function(err, u){
+		if (err){
+			res.send("Error finding User");
+		} else {
+			//u.subscriptions is currently a list of subscriptionIDs.
+			//Want to populate it to be the actual subscription object
+			user.User.populate(u, {path: "subscriptions"}, function(e, result){
+				if (e){
+					res.send("Error populating subscriptions in user");
+				} else {
+					//UI url may change depending on where the view file is
+					res.render('/subscriptions', {subscriptions: u.subscriptions});
+				}
+			}
+		}
+	});
+}
+
+/*POST new subscription (if it does not exist in the database) */
+
 module.exports = router;
