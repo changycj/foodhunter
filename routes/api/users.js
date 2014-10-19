a632a76484c4937db4924bf2bad28f8f9d432b5a
Rebekah Cha <rcha@mit.edu>
2014-10-18 23:11:15 -0400

    made user api populate and return json

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,27 +3,21 @@ var router = express.Router();
 var mongoose = require("mongoose");
 // var location = require("../models/Location");
 var user = require("../models/User.js");
-var subscription = require("../models/Subscription.js");
 
 
 /*GET method for user */
 router.get("/:userID/", function(req, res){
 	//find users by :userID
 	var userID = req.params.userID;
-	user.User.findOne({'_id': userID}, function(err, u){
-		if (err){
-			res.send("Error finding User");
+	user.User.findOne({'_id': userID})
+	//Populate both subscriptions and events field
+	.populate('subscriptions events')
+	.exec(function(err, u){
+		if(err){
+			res.send('Error populating user subscriptions');
 		} else {
-			//u.subscriptions is currently a list of subscriptionIDs.
-			//Want to populate it to be the actual subscription object
-			user.User.populate(u, {path: "subscriptions"}, function(e, result){
-				if (e){
-					res.send("Error populating subscriptions in user");
-				} else {
-					//UI url may change depending on where the view file is
-					res.render('/users/profile', {user: u});
-				}
-			}
+			//returning JSON object
+			res.json(u);
 		}
 	});
 }
