38828653296418994fdfbcd6c045cb095013359a
Yachi Chang <changycj@mit.edu>
2014-10-20 16:53:21 -0400

    test scripts for users and locations api

--- public/javascripts/tests/locations.js
+++ public/javascripts/tests/locations.js
@@ -1,3 +1,48 @@
+// testing Location API
+
+// 1. test GET "/api/locations" to get ALL locations
+// 2. test GET "/api/locations/:loc_id" to get ONE single location
+
 $(document).ready(function() {
-    console.log("TEST LOCATION API!");
+    
+    $.ajax({
+        url: "/api/locations",
+        method: "GET",
+        success: function(data) {
+            testAllLocationsGet(data);
+
+            // assume all successful
+            // randomly takes the 150th entry (we know there are 253 entries)
+            var dummy_loc = data.locations[150];
+            console.log(dummy_loc);
+
+            $.ajax({
+                url: "/api/locations/" + dummy_loc._id,
+                method: "GET",
+                success: function(data) {
+                    testOneLocationGet(data, dummy_loc);
+                }
+            });
+        }
+
+    });
+
+    function testOneLocationGet(data, dummy_loc) {
+        // test getting one
+        test("testing getting one specific location", function() {
+            equal(data.success, 1, "Data successfully returned");
+            equal(data.location._id, dummy_loc._id, "_id's (" + dummy_loc._id + ") are equal.")
+            equal(data.location.building, dummy_loc.building, "building's (" + dummy_loc.building + ") are equal.");
+            equal(data.location.name, dummy_loc.name, "name's (" + dummy_loc.name + ") are equal.");
+            equal((data.location.gps.lat == dummy_loc.gps.lat) && (data.location.gps.lon == dummy_loc.gps.lon), true, 
+                "[lat, lon]'s [" + dummy_loc.gps.lat + ", " + dummy_loc.gps.lon + "] are equal.");
+        });
+    }
+    function testAllLocationsGet(data) {
+        // test getting all 
+        test("testing getting all locations", function() {
+            equal(data.success, 1, "Data successfully returned");
+            equal(data.locations.length > 0, true, "Nonempty list of locations (" + data.locations.length + ") returned");
+        });
+    }
 });
--- public/javascripts/tests/users.js
+++ public/javascripts/tests/users.js
@@ -1,2 +1,61 @@
 $(document).ready(function() {
+
+    $.ajax({
+        url: "/api/users/login",
+        method: "POST",
+        data: {kerberos: "test"},
+        success: function(data) {
+
+            testLogin(data);
+
+            var dummy_user = data.user;
+
+            $.ajax({
+                url: "/api/users/" + data.user._id,
+                method: "GET",
+                success: function(data) {
+                    console.log("HI");
+                    testUserGet(data, dummy_user);
+                }
+            });
+        }
+
+    });
+
+    function compareArrays(a1, a2) {
+        if (a1 instanceof Array && a2 instanceof Array) {
+            if (a1.length == a2.length) {
+                for (var i = 0; i < a1.length; i++) {
+                    if (a1[i] != a2[i]) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    function testUserGet(data, dummy_user) {
+        test("testing user get", function() {
+            equal(data.success, 1, "Data successfully returned.");
+            equal(data.user._id, dummy_user._id, "Username's are equal (" + dummy_user._id+ ").");
+            equal(compareArrays(data.user.subscriptions, dummy_user.subscriptions), true, 
+                "User subscriptions are equal size " + dummy_user.subscriptions.length + " array.");
+            equal(data.user.events instanceof Array, true, 
+                "User events are equal size " + dummy_user.events.length+ " array.");
+        });
+    }
+
+    function testLogin(data) {
+        test("testing user login", function() {
+            equal(data.success, 1, "Data successfully returned.");
+            equal(data.user._id, "test", "Username is correct (test).");
+            equal(data.user.subscriptions instanceof Array, true, 
+                "User subscriptions is a size " + data.user.subscriptions.length + " array.");
+            equal(data.user.events instanceof Array, true, 
+                "User events is a size " + data.user.events.length+ " array.");
+        });
+    }
 });
